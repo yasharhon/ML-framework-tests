ea8c1887bfed695fa7ad13139292f49175fe9c93
Yashar Honarmandi <yashar@entecon.se>
2025-03-25 14:41:34 +0100

    Adjust for working on the digits dataset instead of MNIST

--- jax-tests/digits-test.py
+++ jax-tests/digits-test.py
@@ -3,11 +3,12 @@ import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import optax
-import tensorflow.keras.datasets as dts
+import sklearn.datasets as skldts
+import sklearn.model_selection as sklms
 
 # Define neural network
 class SimpleNN(nnx.Module):
-  def __init__(self, n_features: int = 28*28, n_hidden: int = 100, n_targets: int = 10,
+  def __init__(self, n_features: int = 8*8, n_hidden: int = 100, n_targets: int = 10,
                *, rngs: nnx.Rngs):
     self.n_features = n_features
     self.layer1 = nnx.Linear(n_features, n_hidden, rngs=rngs)
@@ -41,15 +42,18 @@ def train_step(
     data: jax.Array,
     labels: jax.Array
 ):
-    loss_gradient = nnx.grad(loss_fun, has_aux=True)  # gradient transform!
-    grads, logits = loss_gradient(model, data, labels)
-    optimizer.update(grads)  # inplace update
+  loss_gradient = nnx.grad(loss_fun, has_aux=True)  # gradient transform!
+  grads, logits = loss_gradient(model, data, labels)
+  optimizer.update(grads)  # inplace update
 
-# Fetch MNIST dataset
-(x_train_np, y_train_np), (x_test_np, y_test_np) = dts.mnist.load_data()
+# Fetch digits dataset
+digits = skldts.load_digits()
+
+# Split into training and test data
+splits = sklms.train_test_split(digits.images, digits.target, random_state=0)
 
 # Convert to JAX arrays
-x_train, y_train, x_test, y_test = map(jnp.asarray, [x_train_np, y_train_np, x_test_np, y_test_np])
+x_train, x_test, y_train, y_test = map(jnp.asarray, splits)
 
 # Create model
 model = SimpleNN(rngs=nnx.Rngs(0))
@@ -57,7 +61,8 @@ model = SimpleNN(rngs=nnx.Rngs(0))
 # Create optimizer
 optimizer = nnx.Optimizer(model, optax.sgd(learning_rate=0.05))
 
-for i in range(5):
+for i in range(101):
     train_step(model, optimizer, x_train, y_train)
-    loss, _ = loss_fun(model, x_test, y_test)
-    print("Epoch {0}: Loss {1}".format(i, loss))
+    if i % 10 == 1:
+      loss, _ = loss_fun(model, x_test, y_test)
+      print("Epoch {0}: Loss {1}".format(i, loss))
